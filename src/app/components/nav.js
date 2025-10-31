effac9f4f24df39d4dcf414d9d5953f1f0c1b48b
Alfred <deshawn.malone14@gmail.com>
2025-10-30 23:22:53 -0500

    update nav

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -23,13 +23,55 @@ export default function Nav() {
   const [projectsCollapse, setProjectsCollapse] = useState(false);
   return (
     <>
-    <nav className='bg-surface-a10 text-light-a0'>
-        
-        <div className='relative flex flex-row items-center p-5 lg:h-20 md:h-full w-auto'>
-            <div className="rounded-full flex lg:hidden visable w-20 h-20">
-                <img className="w-100" src="https://bucket.theamventure.com/Logo/amv/logo.png" />
+    <nav className='bg-surface-a10 text-light-a0 flex flex-row justify-between h-full w-auto'>
+        <div className="rounded-full flex w-[5%]">
+            <img className="w-100" src="https://bucket.theamventure.com/Logo/amv/logo.png" />
+        </div>
+        <div className='flex flex-row m-auto'>
+            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
+                <p className="font-bold text-lg px-3 my-auto">home</p>
+            </Link >
+            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
+                <p className="font-bold text-lg px-3 my-auto">About</p>
+            </Link >
+            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
+                <p className="font-bold text-lg px-3 my-auto">Portfolio</p>
+            </Link >
+            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
+                <p className="font-bold text-lg px-3 my-auto">Web Services</p>
+            </Link >
+            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
+                <p className="font-bold text-lg px-3 my-auto">Blog</p>
+            </Link >
+
+            {/*general links*/}
+            <div className="py-2  inline-block" onClick={() => setGeneralCollapse(generalCollapse === true ? false : true)}>
+                <div className='flex flex-row px-3'>
+                    <p className='font-bold text-lg pr-1 my-auto'>General</p>
+                        <Lineicons icon={ChevronDownDuotone} size={20} strokeWidth={2} />
+                </div>
+                <ul className={generalCollapse ? 'absolute  ps-10' : `hidden`}>
+                    {
+                                    links.map( (link,i) => 
+                                        <li className='px-4 py-2' key={i} >
+                                            <div className='items-center'>
+                                                <Link className='flex flex-row' href={link.link} target="_blank" rel="noopener noreferrer">
+                                                    <Lineicons icon={link.icon} size={20} strokeWidth={2} />
+                                                    <p className="ml-1 text-lg">{link.name}</p>
+                                                </Link >
+                                            </div>
+                                        </li>
+                                    )
+                    }
+                </ul>
             </div>
-            <p className='text-xl font-bold'>Welcome</p>
+        </div>
+            
+                <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
+                        <p className="font-bold text-lg pr-1 my-auto">button here</p>
+                </Link >
+        
+        <div className={ navCollapse ? 'relative flex flex-row items-center p-5' : "hidden"} >
             <div className='absolute z-40 h-5  flex flex-col w-[80%]'>
                 <div className={navCollapse ? "flex flex-col bg-surface-a10 text-light-a0 mt-8 visible  border-black border-3" : "hidden"}>
                     {/*general links*/}
