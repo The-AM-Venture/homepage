f2f3bd60e91355c773fc53671ba8e15057b83ed8
Alfred <deshawn.malone14@gmail.com>
2025-11-01 15:48:32 -0500

    update nav

--- src/app/components/nav.js
+++ src/app/components/nav.js
@@ -6,8 +6,10 @@ import React, { useState }  from 'react';
 
 const links = [
   {name: 'Home', icon: Home2Duotone, link: 'https://home.theamventure.com'},
-  {name: 'Code', icon: Code1Duotone, link: 'https://code.theamventure.com'},
-  {name: 'Status', icon: BarChart4Duotone, link: 'https://status.theamventure.com'},
+  {name: 'About', icon: Code1Duotone, link: 'https://code.theamventure.com'},
+  {name: 'portfolio', icon: BarChart4Duotone, link: 'https://status.theamventure.com'},
+  {name: 'Web Servies', icon: BarChart4Duotone, link: 'https://status.theamventure.com'},
+  {name: 'Blog', icon: BarChart4Duotone, link: 'https://status.theamventure.com'},
 
 ];
 
@@ -23,26 +25,19 @@ export default function Nav() {
   const [projectsCollapse, setProjectsCollapse] = useState(false);
   return (
     <>
-    <nav className='bg-surface-a10 text-light-a0 flex flex-row justify-between h-full w-auto'>
-        <div className="rounded-full flex w-[5%]">
+    <nav className='relative text-light-a0 flex flex-row justify-between items=center h-full w-auto px-5'>
+        <div className="rounded-full flex lg:w-[5%] w-[15%]">
             <img className="w-100" src="https://bucket.theamventure.com/Logo/amv/logo.png" />
         </div>
-        <div className='flex flex-row m-auto'>
-            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
-                <p className="font-bold text-lg px-3 my-auto">home</p>
-            </Link >
-            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
-                <p className="font-bold text-lg px-3 my-auto">About</p>
-            </Link >
-            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
-                <p className="font-bold text-lg px-3 my-auto">Portfolio</p>
-            </Link >
-            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
-                <p className="font-bold text-lg px-3 my-auto">Web Services</p>
-            </Link >
-            <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
-                <p className="font-bold text-lg px-3 my-auto">Blog</p>
-            </Link >
+        <div className='lg:flex flex-row m-auto hidden'>
+            
+                {
+                    links.map((link,i) => 
+                    <Link className='flex flex-row' href={link.link} target="_blank" rel="noopener noreferrer">
+                        <p className="font-bold text-lg px-3 my-auto">{link.name}</p>
+                    </Link >
+                    )
+                }
 
             {/*general links*/}
             <div className="py-2  inline-block" onClick={() => setGeneralCollapse(generalCollapse === true ? false : true)}>
@@ -67,64 +62,29 @@ export default function Nav() {
             </div>
         </div>
             
-                <Link className='flex flex-row' href={"#"} target="_blank" rel="noopener noreferrer">
-                        <p className="font-bold text-lg pr-1 my-auto">button here</p>
+                <Link className='lg:flex hidden flex-row bg-primary-a0 hover:bg-surface-a0 text-white font-bold py-2 px-4 rounded my-auto' href={"#"} target="_blank" rel="noopener noreferrer">
+                    <p className="font-bold text-lg">Contact</p>  
                 </Link >
-        
-        <div className={ navCollapse ? 'relative flex flex-row items-center p-5' : "hidden"} >
-            <div className='absolute z-40 h-5  flex flex-col w-[80%]'>
+      
+    {/* Mobile view */}
+        <Lineicons onClick={() => setNavCollapse(navCollapse === true ? false : true)} className="ml-auto nav-collapse lg:hidden" icon={MenuHamburger1Duotone} size={40} strokeWidth={2} />
+        <div className={ navCollapse ? 'absolute flex flex-row items-center p-5 w-full' : "hidden"} >
+            <div className=' z-40 flex flex-col w-[80%]'>
                 <div className={navCollapse ? "flex flex-col bg-surface-a10 text-light-a0 mt-8 visible  border-black border-3" : "hidden"}>
-                    {/*general links*/}
-                    <div className="py-2  inline-block" onClick={() => setGeneralCollapse(generalCollapse === true ? false : true)}>
-                        
-                        <div className='flex flex-row ps-10'>
-                            <p className='font-bold text-lg pr-1 my-auto'>General</p>
-                            <Lineicons icon={ChevronDownDuotone} size={20} strokeWidth={2} />
-                        </div>
-                        
-                        <ul className={generalCollapse ? 'block  ps-10' : `hidden`}>
-                                {
-                                    links.map( (link,i) => 
-                                        <li className='px-4 py-2' key={i} >
-                                            <div className='items-center'>
-                                                <Link className='flex flex-row' href={link.link} target="_blank" rel="noopener noreferrer">
-                                                    <Lineicons icon={link.icon} size={20} strokeWidth={2} />
-                                                    <p className="ml-1 text-lg">{link.name}</p>
-                                                </Link >
-                                            </div>
-                                        </li>
-                                    )
-                                }
-                        </ul>
-                    </div>
-                    {/*project links*/}
-                    <div className="py-2 inline-block" onClick={() => setProjectsCollapse(projectsCollapse === true ? false : true)}>
-                        
-                        <div className='flex flex-row ps-10'>
-                            <p className='font-bold text-lg pr-1 my-auto'>Projects</p>
-                            <Lineicons icon={ChevronDownDuotone} size={20} strokeWidth={2} />
-                        </div>
-                        
-                        <ul className={projectsCollapse ? 'block ps-10' : `hidden`}>
-                                {
-                                    pLinks.map( (link,i) => 
-                                        <li className='px-4 py-2' key={i} >
-                                            <div className='items-center'>
-                                                <Link className='flex flex-row' href={link.link} target="_blank" rel="noopener noreferrer">
-                                                    <Lineicons icon={link.icon} size={20} strokeWidth={2} />
-                                                    <p className="ml-1 text-lg">{link.name}</p>
-                                                </Link >
-                                            </div>
-                                        </li>
-                                    )
-                                }
-                        </ul>
-                    </div>
-
+                    {
+                    links.map((link,i) => 
+                    <Link className='flex flex-row' href={link.link} target="_blank" rel="noopener noreferrer">
+                        <p className="font-bold text-lg px-3 my-auto">{link.name}</p>
+                    </Link >
+                    )
+                    }
                 </div>
 
-            </div>
-                        <Lineicons onClick={() => setNavCollapse(navCollapse === true ? false : true)} className="ml-auto nav-collapse lg:hidden" icon={MenuHamburger1Duotone} size={40} strokeWidth={2} />
+                <Link className='flex flex-row bg-primary-a0 hover:bg-surface-a0 text-white font-bold py-2 px-4 rounded my-auto mt-[2%]' href={"#"} target="_blank" rel="noopener noreferrer">
+                    <p className="font-bold text-lg">Contact</p>  
+                </Link >
+
+            </div>      
 
         </div>
         
